4cc5a7055cc57555236d0615428517f783f64902
Matias Cordova <97194679+TheRahim@users.noreply.github.com>
2022-03-02 00:27:01 -0300

    Delete forms.py

--- proyecto_practica/apps/forms.py
+++
@@ -1,8 +0,0 @@
-from socket import fromshare
-from django import forms
-from .models import *
-
-class RitForm(forms.ModelForm):
-    class Meta:
-        model=Rit
-        fields=('letra','nro_causa','anio')
